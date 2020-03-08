41823982881a1b802bd96e2e23128f700c41f2bf
toptechtips <toptechtips497@gmail.com>
2020-03-08 17:45:09 +0000

    favicon seem to just not work, so I will manuall add it

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Layout, SEO } from 'components/common';
 import { Intro, Skills, Contact, Projects } from 'components/landing';
+import { withPrefix } from 'gatsby';
 
 export default () => (
   <Layout>
+    <link rel="shortcut icon" type="image/x-icon" href={withPrefix("/") + "favicon/favicon-512.png"}></link>
     <SEO />
     <Intro />
     <Projects />
