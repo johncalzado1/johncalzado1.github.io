45ff9c0a392e44856fc49c1dc2837704619ba431
toptechtips <toptechtips497@gmail.com>
2020-03-14 20:41:26 +0000

    changed some info on portfolio

--- src/components/theme/Header/Navbar/index.jsx
+++ src/components/theme/Header/Navbar/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, withPrefix } from 'gatsby'
 import { Container } from 'components/common'
 import NavbarLinks from '../NavbarLinks'
 import { Wrapper } from './styles'
 
 const Navbar = () => (
 	<Wrapper as={Container}>
-		<Link to="/">John Calzado</Link>
+		<Link to="/">
+			<img src={withPrefix("/") + "favicon/favicon-512.png"}></img>
+		</Link>
 		<NavbarLinks desktop />
 	</Wrapper>
 )
